da1660c367caef0280139d77b417856148617d4c
jentle <jgu7@ncsu.edu>
2015-02-05 21:47:05 -0500

    Delete unused static import and test case

--- src/test/java/com/webapp/dao/impl/mdb/BaseMDBImplTest.java
+++ src/test/java/com/webapp/dao/impl/mdb/BaseMDBImplTest.java
@@ -8,8 +8,6 @@ import javax.annotation.Resource;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.annotation.Repeat;
 import org.springframework.test.annotation.Rollback;
@@ -23,23 +21,12 @@ import com.webapp.model.User;
 public class BaseMDBImplTest extends SpringTransactionContextTest{
 	
 	@Resource(name = "baseMDBImpl")
-    private BaseMDBImpl<User> mongoDao;
-	
-	/**
-	 *  Initialization of Spring Context , Get Bean from ApplicationContext 
-	 *
-	@BeforeClass
-	public static void init(){
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("/META-INF/mongo-config.xml");
-		mongoDao = (BaseMDBImpl<User>) ctx.getBean("baseMDBImpl");
-	}
-	*/
+        private BaseMDBImpl<User> mongoDao;
 	
 	@Test
 	//@Rollback(true)
 	public void save(){	
 		User u = new User("jesse",123);
-		mongoDao.save(u);
 		mongoDao.save(u);
 	}
 	
@@ -55,13 +42,4 @@ public class BaseMDBImplTest extends SpringTransactionContextTest{
 		userList.add(c);
 		mongoDao.save(userList);
 	}
-	
-	
-//	@Test
-//	@Rollback(true)
-//	public void deletebyID(){		
-//		int id=1123;
-//		mongoDao.delete(user);
-//	}
-//	
 }
