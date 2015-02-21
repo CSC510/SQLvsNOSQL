548b92497c1f23b060f103ae6c950895c46ad4f5
Jesse <jesse.xu.20@gmail.com>
2015-02-21 15:22:03 -0500

    add component annotation

--- src/main/java/com/webapp/service/RequestService.java
+++ src/main/java/com/webapp/service/RequestService.java
@@ -2,13 +2,18 @@ package com.webapp.service;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
 import com.webapp.dao.HouseDao;
 import com.webapp.dao.RequestDao;
 import com.webapp.dao.UserDao;
 import com.webapp.model.House;
 import com.webapp.model.Request;
-
+@Component
 public class RequestService {
+	@Resource(name = "requestMDBImpl")
 	private RequestDao requestDao;
 	private UserDao userDao;
 	private HouseDao houseDao;
--- src/test/java/com/webapp/dao/service/mdb/RequestServiceTest.java
+++ src/test/java/com/webapp/dao/service/mdb/RequestServiceTest.java
@@ -3,13 +3,14 @@ package com.webapp.dao.service.mdb;
 import javax.annotation.Resource;
 
 import org.junit.Test;
+import org.springframework.stereotype.Component;
 
 import com.webapp.common.test.SpringTransactionContextTest;
 import com.webapp.model.House;
 import com.webapp.model.Request;
 import com.webapp.model.User;
 import com.webapp.service.RequestService;
-
+@Component
 public class RequestServiceTest extends SpringTransactionContextTest {
 	
 	@Resource
