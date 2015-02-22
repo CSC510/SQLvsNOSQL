b1f01b0686eb9a24f3ef542dc6086761a872e674
maxlpy <maxlipengyu@gmail.com>
2015-02-22 16:28:23 -0500

    modify HouseDao and UserDao

--- src/main/java/com/webapp/dao/HouseDao.java
+++ src/main/java/com/webapp/dao/HouseDao.java
@@ -2,8 +2,10 @@ package com.webapp.dao;
 
 import java.util.List;
 
+import com.webapp.daoimpl.mdb.Parameter;
 import com.webapp.model.House;
 
 public interface HouseDao extends BaseDao<House> {
 	public List<House> findByType(String typeString);
+	public List<House> findAll(Parameter parameter);
 }
