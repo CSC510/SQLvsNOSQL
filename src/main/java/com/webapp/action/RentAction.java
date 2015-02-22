a863e3c10f95b02c91a9f01c1b0a59c898f6ed6d
maxlpy <maxlipengyu@gmail.com>
2015-02-22 14:31:04 -0500

    fix bugs

--- src/main/java/com/webapp/action/RentAction.java
+++ src/main/java/com/webapp/action/RentAction.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import com.webapp.model.House;
 import com.webapp.model.Request;
@@ -13,6 +14,7 @@ import com.webapp.service.HouseService;
 import com.webapp.service.RequestService;
 import com.webapp.service.UserService;
 
+@Controller
 public class RentAction {
 	@Autowired
 	private RequestService requestService;
