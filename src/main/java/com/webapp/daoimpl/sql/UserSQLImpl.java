167249eb6324f8f643e7b97c06903b277d71226a
maxlpy <maxlipengyu@gmail.com>
2015-02-22 16:31:25 -0500

    modify UserSQLImpl

--- src/main/java/com/webapp/daoimpl/sql/UserSQLImpl.java
+++ src/main/java/com/webapp/daoimpl/sql/UserSQLImpl.java
@@ -3,12 +3,19 @@ package com.webapp.daoimpl.sql;
 import java.util.List;
 
 import com.webapp.dao.UserDao;
+import com.webapp.daoimpl.mdb.Parameter;
 import com.webapp.model.User;
 
 public class UserSQLImpl extends BaseSQLImpl<User> implements UserDao{
 
 	@Override
 	public List<User> findByName(String name) {
+		return null;
+	}
+
+	@Override
+	public User findOne(Parameter parameter) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
