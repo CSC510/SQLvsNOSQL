15676aae5227ce1863bf8f99e09fa1ab336554a3
maxlpy <maxlipengyu@gmail.com>
2015-02-21 23:19:27 -0500

    add UserService methods

--- src/main/java/com/webapp/service/UserService.java
+++ src/main/java/com/webapp/service/UserService.java
@@ -4,25 +4,46 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.webapp.dao.UserDao;
+import com.webapp.daoimpl.mdb.Parameter;
 import com.webapp.model.User;
 
 @Component
 public class UserService {
 	@Resource(name = "userMDBImpl")
+	@Autowired
 	private UserDao userDao;
 	
 	public UserService(){
 		
 	}
 	
-	public void addUser(User u){
+	public void saveUser(User u){
 		userDao.save(u);
 	}
 	
-	public List<User> findUerByName(String name){
+	public List<User> findUser(String name){
 		return userDao.findByName(name);
+	}
+	
+	public void deleteUserByName(String name) {
+		List<User> users = userDao.findByName(name);
+		for(User user: users) {
+			userDao.delete(user);
+		}
+	}
+	
+	public void deleteByStudentId(int studentId) {
+		Parameter parameter = new Parameter();
+		parameter.put("studentId", studentId);
+		User u = (User)userDao.findOne(parameter);
+		userDao.delete(u);
+	}
+	
+	public void deleteUser(User u) {
+		userDao.delete(u);
 	}
 }
