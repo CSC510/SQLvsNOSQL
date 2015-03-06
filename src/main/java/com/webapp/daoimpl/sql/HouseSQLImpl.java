8b98ccf08543e48db1b6235b02bac76dc52fd2eb
maxlpy <maxlipengyu@gmail.com>
2015-03-06 17:05:48 -0500

    add HouseSQLImpl

---
+++ src/main/java/com/webapp/daoimpl/sql/HouseSQLImpl.java
@@ -0,0 +1,28 @@
+package com.webapp.daoimpl.sql;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.webapp.dao.HouseDao;
+import com.webapp.daoimpl.mdb.Parameter;
+import com.webapp.model.House;
+import com.webapp.model.User;
+
+@Component
+public class HouseSQLImpl extends BaseSQLImpl<House> implements HouseDao {
+
+	@Override
+	public List<House> findByType(String typeString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<House> findAll(Parameter parameter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
